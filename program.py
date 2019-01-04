ea0dcde07fe40673b65dc8998d49e7c600c27779
badFarmer <guy@strategicmfg.com>
2019-01-03 20:34:56 -0800

    Initial Commit

--- program.py
+++ program.py
@@ -1,1 +1,13 @@
-print()
+
+def main():
+    # print the header
+    # get zipcode from user
+    # get html from web
+    # parse the html
+    # display the forcast
+    print("hello from main")
+
+
+if __name__ == '__main__':
+    main()
+
