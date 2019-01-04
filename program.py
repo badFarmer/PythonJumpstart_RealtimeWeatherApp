4bb9e61fe002196f78e5de547dfc7f502abe8f56
badFarmer <guy@strategicmfg.com>
2019-01-03 20:46:12 -0800

    Incremental Commit

--- program.py
+++ program.py
@@ -2,7 +2,10 @@
 def main():
     # print the header
     print_the_header()
-    code = input('What zipcode do you want the weather for (97232)?')
+    code = input("What zipcode do you want the weather for (97232)?")
+    code = code.strip()
+    get_html_from_web(code)
+
     # get html from web
     # parse the html
     # display the forcast
@@ -15,7 +18,10 @@ def print_the_header():
     print('-----------------------')
 
 
+def get_html_from_web(zipcode):
 
+    url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
+    print(url)
 
 
 if __name__ == '__main__':
