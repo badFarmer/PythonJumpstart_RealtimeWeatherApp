b45bea35eb4f5669ecfa61786539799db71de158
badFarmer <guy@strategicmfg.com>
2019-01-03 20:37:31 -0800

    Incremental Commit

--- program.py
+++ program.py
@@ -1,11 +1,21 @@
 
 def main():
     # print the header
-    # get zipcode from user
+    print_the_header()
+    code = input('What zipcode do you want the weather for (97232)?')
     # get html from web
     # parse the html
     # display the forcast
     print("hello from main")
+
+
+def print_the_header():
+    print('-----------------------')
+    print('------weather app------')
+    print('-----------------------')
+
+
+
 
 
 if __name__ == '__main__':
