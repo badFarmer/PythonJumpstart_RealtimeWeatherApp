462bb41373ca8c5cbdd2e7b1d0f4941bd36ad721
badFarmer <guy@strategicmfg.com>
2019-01-04 08:59:50 -0800

    Incremental Commit

--- program.py
+++ program.py
@@ -1,15 +1,13 @@
 import requests
-
+import bs4
 
 def main():
     # print the header
     print_the_header()
     code = input("What zipcode do you want the weather for (97232)?")
     code = code.strip()
-    get_html_from_web(code)
-
-    # get html from web
-    # parse the html
+    html = get_html_from_web(code)
+    get_weather_from_html(html)
     # display the forcast
     print("hello from main")
 
@@ -23,8 +21,12 @@ def print_the_header():
 def get_html_from_web(zipcode):
 
     url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
-    #print(url)
     response = requests.get(url)
+    return response.text
+
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+
 
 if __name__ == '__main__':
     main()
